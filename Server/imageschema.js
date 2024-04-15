c310cc04623c5fe3c1f9b65e48b6219c9905dbd9
Bhumika-Raut <bhumikaraut59620@gmail.com>
2024-04-15 14:01:46 +0530

    add image section

--- Server/imageschema.js
+++ Server/imageschema.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
 const ImageSchema = new mongoose.Schema({
-    
-        name: String,
-        image: String,
-        description: String,
-    
+    name: String,
+    image: String,
+    description: String
 });
 
 const ImageEntity = mongoose.model('images-collections', ImageSchema);
 
-module.exports = ImageEntity;
+module.exports = {
+    ImageEntity
+};
